a3714b58e0dfe61db16342d385379528b6c938e8
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2021-10-11 09:17:20 +0100

    Adds holding page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <img src="/ark-logo-temp.png" alt="Ark Labs logo" />
+    <div>
+      <h1 class="text-3xl text-indigo-500">Ark Labs - coming soon</h1>
+      <p class="text-lg text-gray-400">The last website you'll ever need</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
