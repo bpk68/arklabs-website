ad5fb359816e3600ed3708c45b40f71e7a89bbc1
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2021-10-24 14:34:25 +0100

    Adds Ark branding and temp holding page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-    <img src="/ark-logo-temp.png" alt="Ark Labs logo" />
-    <div>
-      <h1 class="text-3xl text-indigo-500">Ark Labs - coming soon</h1>
-      <p class="text-lg text-gray-400">The last website you'll ever need</p>
+  <div class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0">
+    <div class="text-center">
+      <img src="/ark-labs-logo-sm.png" alt="Ark Labs logo" />
+      <h1 class="text-3xl pt-7">Ark Labs - coming soon</h1>
+      <p class="text-lg">Jamstack experts. Shopify consultancy.</p>
     </div>
   </div>
 </template>
@@ -13,3 +13,15 @@ import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
+
+<style>
+@import url(//db.onlinewebfonts.com/c/19966dc0c2f7f41f95c0d8ec32a06d98?family=Code+Pro+Bold);
+
+html, body {
+  background-color: #243544;
+  color: #fdfaf6 !important;
+}
+h1, h2, h3 {
+  font-family: "Code Pro Bold";
+}
+</style>
