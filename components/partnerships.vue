4710c5cea8d754c4245302a9c976a6bbd51a357b
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2022-03-02 09:56:11 +0000

    Adds partnership section

---
+++ components/partnerships.vue
@@ -0,0 +1,34 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="bg-white">
+    <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-32 lg:px-8">
+      <div class="text-center">
+        <h2 class="text-3xl font-extrabold text-brand-blue-900 sm:text-4xl">
+          Partnership seal of approval
+        </h2>
+        <p class="mt-3 mx-auto text-xl text-gray-500 sm:mt-4">
+          We've proven our skills in order to partner with the best e-commerce
+          and marketing platforms in the business
+        </p>
+      </div>
+      <div
+        class="mt-6 sm:grid sm:grid-cols-2 gap-0.5 lg:mt-8 justify-items-center items-center"
+      >
+        <div class="">
+          <img
+            class=""
+            src="/partners/klaviyo.png"
+            alt="Official Klaviyo partner"
+          />
+        </div>
+        <div class="text-center">
+          <img
+            class="max-w-[80%] block mx-auto"
+            src="/partners/shopify-partners.png"
+            alt="Certified Shopify partner"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,48 @@
 <template>
   <div>
-    <div class="pt-10 bg-brand-blue-900 sm:pt-16 lg:pt-8 lg:pb-14 md:overflow-hidden">
+    <div
+      class="pt-10 bg-brand-blue-900 sm:pt-16 lg:pt-8 lg:pb-14 md:overflow-hidden"
+    >
       <div class="mx-auto max-w-7xl lg:px-8">
         <div class="lg:grid lg:grid-cols-2 lg:gap-8">
-          <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+          <div
+            class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center"
+          >
             <div class="lg:py-24">
-              <h1 class="mt-4 text-4xl font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-5xl">
-                <span class="block">A Yorkshire <span class="underline" style="text-decoration-color: #E62B4A;">Shopify agency</span> who create</span>
-                <span class="block text-brand-blue-50">e-commerce solutions that convert</span>
+              <h1
+                class="mt-4 text-4xl font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-5xl"
+              >
+                <span class="block"
+                  >A Yorkshire
+                  <span class="underline" style="text-decoration-color: #e62b4a"
+                    >Shopify agency</span
+                  >
+                  who create</span
+                >
+                <span class="block text-brand-blue-50"
+                  >e-commerce solutions that convert</span
+                >
               </h1>
-              <p class="mt-3 text-base text-brand-cream sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                We're a close-knit team of expert Shopify developers, designers and marketing strategists.
+              <p
+                class="mt-3 text-base text-brand-cream sm:mt-5 sm:text-xl lg:text-lg xl:text-xl"
+              >
+                We're a close-knit team of expert Shopify developers, designers
+                and marketing strategists.
               </p>
               <div class="mt-10 sm:mt-12">
                 <div class="sm:flex sm:justify-center lg:justify-start">
                   <div class="sm:w-1/3">
                     <a
-                        href="#intro"
-                        class="transition-all duration-300 ease-in-out block w-full py-3 px-4 border border-2 border-brand-red-900 rounded-md shadow bg-brand-red-900 text-white text-lg font-medium hover:bg-brand-red-500 hover:border-brand-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-red-900"
+                      href="#intro"
+                      class="transition-all duration-300 ease-in-out block w-full py-3 px-4 border border-2 border-brand-red-900 rounded-md shadow bg-brand-red-900 text-white text-lg font-medium hover:bg-brand-red-500 hover:border-brand-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-red-900"
                     >
                       Find out more
                     </a>
                   </div>
                   <div class="mt-3 sm:mt-0 sm:ml-3 sm:w-1/3">
                     <NuxtLink
-                        to="/contact"
-                        class="transition-all duration-300 ease-in-out block w-full py-3 px-4 rounded-md shadow border border-2 border-brand-cream font-bold text-brand-cream text-lg font-medium hover:bg-brand-cream hover:text-brand-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-cream focus:ring-offset-gray-900"
+                      to="/contact"
+                      class="transition-all duration-300 ease-in-out block w-full py-3 px-4 rounded-md shadow border border-2 border-brand-cream font-bold text-brand-cream text-lg font-medium hover:bg-brand-cream hover:text-brand-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-cream focus:ring-offset-gray-900"
                     >
                       Let's talk
                     </NuxtLink>
@@ -35,8 +52,14 @@
             </div>
           </div>
           <div class="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
-            <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-              <img class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="/online-shopping.svg" alt="Ark Labs Shopify Experts displaying an e-commerce website on a mobile device" />
+            <div
+              class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0"
+            >
+              <img
+                class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                src="/online-shopping.svg"
+                alt="Ark Labs Shopify Experts displaying an e-commerce website on a mobile device"
+              />
             </div>
           </div>
         </div>
@@ -46,6 +69,7 @@
     <HomeIntro />
     <HomeCaseStudies />
     <ServicesBlock />
+    <Partnerships />
     <OurApproach />
     <PartnersBlock />
     <Testimonials />
@@ -56,10 +80,12 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import { generateSocialMeta} from '~/lib/utils';
+import { generateSocialMeta } from '~/lib/utils';
 
-const title = 'Shopify design, migration & marketing in York and Leeds | Headless e-commerce | Ark Labs';
-const description = 'At Ark Labs, we\'re a Shopify partner agency who build Shopify websites for clients in Selby, Leeds, York, Yorkshire';
+const title =
+  'Shopify design, migration & marketing in York and Leeds | Headless e-commerce | Ark Labs';
+const description =
+  "At Ark Labs, we're a Shopify partner agency who build Shopify websites for clients in Selby, Leeds, York, Yorkshire";
 
 export default Vue.extend({
   head() {
@@ -67,13 +93,13 @@ export default Vue.extend({
       title,
       meta: [
         {
-          hid: "description",
-          name: "description",
-          content: description
+          hid: 'description',
+          name: 'description',
+          content: description,
         },
         ...generateSocialMeta(title, description, this?.$route?.path),
-      ]
-    }
-  }
+      ],
+    };
+  },
 });
 </script>
